d7f180a5f8c50485bbc7fc1efcb6166f95df97f4
root <root@6d32ac059abe>
2020-12-24 00:56:55 +0000

    Fibonacci Done! :)

--- src/_01_algorithms/_2_fibonacci/fibonacci.java
+++ src/_01_algorithms/_2_fibonacci/fibonacci.java
@@ -1,5 +1,20 @@
 package _01_algorithms._2_fibonacci;
 
 public class fibonacci {
-
+public static void main(String[] args) {
+	int candy1=0;
+	int cane2=1;
+	int gingerbread=candy1+cane2;
+	System.out.println(candy1);
+	System.out.println(cane2);
+	for(int house=0;house<20;house++) {
+		System.out.println(gingerbread);
+		candy1=cane2;
+		cane2=gingerbread;
+		gingerbread=candy1+cane2;
+	}
+	
+	
+	
+}
 }
